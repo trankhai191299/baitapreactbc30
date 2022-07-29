513df1e765f96ff056443d21e6777af73abc8ca3
khaitran <akashikazuo99@gmail.com>
2022-07-29 15:48:17 +0700

    bài tập react 2: xong

--- src/State/BaiTapGlasses/BaiTapGlasses.jsx
+++ src/State/BaiTapGlasses/BaiTapGlasses.jsx
@@ -1,97 +1,102 @@
 import React, { Component } from "react";
 import "./css/BaiTapGlassess.css";
-const data1 = [
+const data = [
   {
     id: 1,
     price: 30,
     name: "GUCCI G8850U",
-    url: "./glassesImage/v1.png",
+    url: "./img/glassesImage/v1.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 2,
     price: 50,
     name: "GUCCI G8759H",
-    url: "./glassesImage/v2.png",
+    url: "./img/glassesImage/v2.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 3,
     price: 30,
     name: "DIOR D6700HQ",
-    url: "./glassesImage/v3.png",
+    url: "./img/glassesImage/v3.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 4,
     price: 70,
     name: "DIOR D6005U",
-    url: "./glassesImage/v4.png",
+    url: "./img/glassesImage/v4.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 5,
     price: 40,
     name: "PRADA P8750",
-    url: "./glassesImage/v5.png",
+    url: "./img/glassesImage/v5.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 6,
     price: 60,
     name: "PRADA P9700",
-    url: "./glassesImage/v6.png",
+    url: "./img/glassesImage/v6.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 7,
     price: 80,
     name: "FENDI F8750",
-    url: "./glassesImage/v7.png",
+    url: "./img/glassesImage/v7.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 8,
     price: 100,
     name: "FENDI F8500",
-    url: "./glassesImage/v8.png",
+    url: "./img/glassesImage/v8.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
   {
     id: 9,
     price: 60,
     name: "FENDI F4300",
-    url: "./glassesImage/v9.png",
+    url: "./img/glassesImage/v9.png",
     desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
   },
 ];
-const data2 = [
-  { id: 1, img: "./img/glassesImage/g1.jpg" },
-  { id: 2, img: "./img/glassesImage/g2.jpg" },
-  { id: 3, img: "./img/glassesImage/g3.jpg" },
-  { id: 4, img: "./img/glassesImage/g4.jpg" },
-  { id: 5, img: "./img/glassesImage/g5.jpg" },
-  { id: 6, img: "./img/glassesImage/g6.jpg" },
-  { id: 7, img: "./img/glassesImage/g7.jpg" },
-  { id: 8, img: "./img/glassesImage/g8.jpg" },
-  { id: 9, img: "./img/glassesImage/g9.jpg" },
-];
+
 export default class BaiTapGlasses extends Component {
+  state = {
+    thongTinMatKinh :{
+      name:"GUCCI G8850U",
+      url:"./img/glassesImage/v1.png",
+      desc:"Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. "
+    },
+  }
   renderGlasses = () => {
-    return data2.map((glassImg, index) => {
-      return (
-        <div className="col-2" key={index}>
-          <div className="item p-3">
-            <img src={glassImg.img} alt="..." className="img-fluid" />
+      return data.map((glassInform, index) => {
+        return (
+          <div className="col-2" key={index}>
+            <div className="item p-3">
+              <img src={glassInform.url} alt="..." className="img-fluid" onClick={()=>{
+                this.changeGlasses(glassInform)
+              }}/>
+            </div>
           </div>
-        </div>
-      );
-    });
+        );
+      });
   };
+  changeGlasses = (sanphamClick) => {
+    this.setState({
+      thongTinMatKinh : sanphamClick,
+    })
+  }
   render() {
+    let {name,url,desc} = this.state.thongTinMatKinh
     return (
       <div className="baiTapGlass_Body">
-        <div className="overlay"></div>
+        {/* <div className="overlay"></div> */}
         <header
           className="text-center p-4 text-white"
           style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
@@ -103,13 +108,22 @@ export default class BaiTapGlasses extends Component {
         <div className="main-content pt-5">
           <div className="container">
             <div className="model-section pb-4">
-              <div className="row">
+              <div className="row align-items-center justify-content-center">
                 <div className="col-6">
-                  <img
-                    src="/img/glassesImage/model.jpg"
-                    alt="..."
-                    height={250}
-                  />
+                  <div className="image">
+                    <img
+                      src="/img/glassesImage/model.jpg"
+                      alt="..."
+                      height={250}
+                    />
+                    <div className="overlay-content">
+                      <img src={url} alt="..." />
+                      <div className="information">
+                        <h2 className="glass-name">{name}</h2>
+                        <p className="glass-description">{desc}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className="col-6">
                   <img
@@ -123,33 +137,6 @@ export default class BaiTapGlasses extends Component {
             <div className="glasses-choice p-3 mt-5 bg-white">
               <div className="row align-items-center">
                 {this.renderGlasses()}
-                {/* <div className="col-2">
-                            <img src="/img/glassesImage/g1.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g2.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g3.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g4.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g5.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g6.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g7.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g8.jpg" alt="" className='img-fluid'/>
-                        </div>
-                        <div className="col-2">
-                            <img src="/img/glassesImage/g9.jpg" alt="" className='img-fluid'/>
-                        </div> */}
               </div>
             </div>
           </div>
