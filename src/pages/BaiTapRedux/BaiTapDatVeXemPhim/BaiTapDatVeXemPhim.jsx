1af037adb82e9fc22bc12b8993cdb20e9a5cfefd
khaitran <akashikazuo99@gmail.com>
2022-08-26 20:25:24 +0700

    dan layout(1) cho bai dat ve

--- src/pages/BaiTapRedux/BaiTapDatVeXemPhim/BaiTapDatVeXemPhim.jsx
+++ src/pages/BaiTapRedux/BaiTapDatVeXemPhim/BaiTapDatVeXemPhim.jsx
@@ -1,20 +1,44 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import './BaiTapBookingTicket.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./BaiTapBookingTicket.css";
+import ThongTinDatGhe from "./ThongTinDatGhe";
 
 class BaiTapDatVeXemPhim extends Component {
   render() {
     return (
-      <div className='position-fixed w-100 h-100' style={{backgroundImage:"url('./img/bookingTicket/bgmovie.jpg')",backgroundSize:'100%',backgroundRepeat:"no-repeat"}}>
-        <div style={{backgroundColor:"rgba(0,0,0,0.8)"}} className='position-fixed w-100 h-100'>
-
+      <div
+        className="bookingMovie position-fixed w-100 h-100"
+        style={{
+          backgroundImage: "url('./img/bookingTicket/bgmovie.jpg')",
+          backgroundSize: "100%",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div
+          style={{ backgroundColor: "rgba(0,0,0,0.8)" }}
+          className="position-fixed w-100 h-100"
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-8 text-center">
+                <h1 className="text-warning text-center">Bài tập đặt vé xem phim</h1>
+                <h4 className="mt-5 text-light">Màn hình</h4>
+                <div className="d-flex flex-row justify-content-center mt-1">
+                  <div className="screen"></div>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="text-light fs-3 mt-2 text-center">Danh sách bạn chọn</div>
+                <ThongTinDatGhe/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({})
+const mapStateToProps = (state) => ({});
 
-
-export default connect(mapStateToProps)(BaiTapDatVeXemPhim)
+export default connect(mapStateToProps)(BaiTapDatVeXemPhim);
--- src/pages/BaiTapRedux/BaiTapDatVeXemPhim/ThongTinDatGhe.jsx
+++ src/pages/BaiTapRedux/BaiTapDatVeXemPhim/ThongTinDatGhe.jsx
@@ -4,8 +4,39 @@ import { connect } from 'react-redux'
 class ThongTinDatGhe extends Component {
   render() {
     return (
-      <div>ThongTinDatGhe</div>
-    )
+      <div>
+        <div className="mt-5">
+          <div className="d-flex flex-column">
+            <div className="btnDcChon">
+              <button className="gheDuocChon me-2"></button>
+              <span className="text-light">Ghế đã đặt</span>
+            </div>
+            <div className="btnDgChon">
+              <button className="gheDangChon me-2"></button>
+              <span className="text-light">Ghế đang đặt</span>
+            </div>
+            <div className="btnChChon">
+              <button className="ghe me-2" style={{ marginLeft: 0 }}></button>
+              <span className="text-light">Ghế chưa đặt</span>
+            </div>
+          </div>
+        </div>
+        <div className='mt-5'>
+          <table className="table" border="2">
+            <thead>
+              <tr className='text-light fs-5'>
+                <th>Số ghế</th>
+                <th>Giá</th>
+                <th>Hủy</th>
+              </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
 }
 
